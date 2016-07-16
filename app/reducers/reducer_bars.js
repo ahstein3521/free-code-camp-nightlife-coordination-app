bdfbee354f8dc097edb39e8daf28bd2f6c6f98a9
ahs3521 <adamhs3521@gmail.com>
2016-07-15 23:49:56 -0400

    Update reducer_bars.js

--- app/reducers/reducer_bars.js
+++ app/reducers/reducer_bars.js
@@ -1,12 +1,11 @@
 import { FETCH_BARS,RSVP } from '../actions/types';
 
-export default function(state =null, action) {
- 
-  switch (action.type) {
-  case FETCH_BARS:
-    return action.payload;
-  case RSVP:
-  	return Object.assign({},{...state,...action.payload})  
+export default function(state=null, action) {
+ 	switch (action.type) {
+  	case FETCH_BARS:
+    	return action.payload;
+  	case RSVP:
+  		return {...state,...action.payload}  
   }
   return state;
 }
