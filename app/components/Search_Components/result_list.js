fde74c2126ad81b11ae1182c614bf4f0f6af665f
ahs3521 <adamhs3521@gmail.com>
2016-07-15 23:29:22 -0400

    Update result_list.js

--- app/components/Search_Components/result_list.js
+++ app/components/Search_Components/result_list.js
@@ -9,13 +9,15 @@ const List=(props)=>{
    const list= props.result.venues.map((v,i)=>{
       return <Venue BAR={v} key={i} RSVP={props.RSVP}/>
     })
+    
     return(
       <div className='search-results'>
-        <div className='search-results-title'><h5>Top Results For {props.result.location}</h5></div>
-        <ul className='list-group'>{list}</ul>
+         <div className='search-results-title'>
+            <h5>Top Results For {props.result.location}</h5>
+         </div>
+         <ul className='list-group'>{list}</ul>
       </div>
     )
-
 } 
 export default List;
     
